e0464f3db48ae24fcfd9112a8f7980c856747f6c
nniimma <nimakhazforoosh@yahoo.com>
2023-12-04 11:50:23 -0300

    making a cart object for adding

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,8 @@ products.forEach((product) => {
             Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-name="${product.name}">
             Add to Cart
             </button>
         </div>
@@ -55,3 +56,15 @@ products.forEach((product) => {
 
 
 document.querySelector('.products-grid'). innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productName = button.dataset.productName;
+        cart.push({
+            productName: productName,
+            quantity: 1
+        })
+
+        console.log(cart)
+    })
+})
