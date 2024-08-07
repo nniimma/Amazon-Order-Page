6895b988d7d3166d62fb7b761b242a98bc6d7ea4
Nima <alireza.khazforoosh@adventistas.org>
2024-08-07 09:17:40 -0300

    procedural (functional) programming vs OOP -functional: step by step -OOP (object oriented programmin): using objects to program -in oop we should use PascalCase

---
+++ data/cart-oop.js
@@ -0,0 +1,99 @@
+// a function to make objects, if we need to have more than one cart in future:
+function Cart(localStorageKey){
+    //! 'this' give the outer object name, this helps when changing the name of the object you don't need to change the names inside.
+    const cart = {
+        cartItems: undefined,
+
+        //! funciton inside an object is a method
+        loadFromStorage: function () {
+            this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
+            if (!this.cartItems){
+                this.cartItems = []
+            }
+        },
+
+        //! shorthand of method: (the same code as above)
+        saveToStorage () {
+            localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems))
+        },
+
+        addToCart (productID) {
+            
+            let matchingItem;
+            this.cartItems.forEach((cartItem) =>{
+                if (productID === cartItem.ID){
+                    matchingItem = cartItem
+                }
+            })
+        
+            const productQuantity = document.querySelector(`.js-quantity-selector-${ productID }`).value
+            
+            if(matchingItem){
+                matchingItem.quantity = Number(productQuantity) + Number(matchingItem.quantity) 
+            }else{            
+                this.cartItems.push({
+                    ID: productID,
+                    quantity: Number(productQuantity),
+                    deliveryOptionsId: '1'
+                })
+            }
+
+            this.saveToStorage()
+        
+            document.querySelector(`.js-added-to-cart-${ productID }`).classList.add('added-to-cart-active')
+            
+            setTimeout(function(){
+                document.querySelector(`.js-added-to-cart-${productID}`).classList.remove('added-to-cart-active');
+            }, 2000);
+        },
+
+        updateCartQuantity () {
+            let totalQuantity = 0
+            this.cartItems.forEach((item) => {
+                totalQuantity += item.quantity
+            })
+            
+            document.querySelector('.return-to-home-link').innerHTML = totalQuantity + ' ' + 'items'
+        },
+
+        getTotalQuantity () {
+            let totalQuantity = 0
+            this.cartItems.forEach((item) => {
+                totalQuantity += item.quantity
+            })
+            return totalQuantity
+        },
+
+        removeFromCart(productID){
+            let newCart = []
+        
+            this.cartItems.forEach((cartItem) => {
+                if (cartItem.ID !== productID){
+                    newCart.push(cartItem)
+                }
+            })
+            this.cartItems = newCart
+            this.saveToStorage()
+            this.updateCartQuantity()
+        },
+        
+        updateDeliveryOption(productId, deliveryOptionId){
+            let matchingItem;
+            this.cartItems.forEach((cartItem) =>{
+                if (productId === cartItem.ID){
+                    matchingItem = cartItem
+                }
+            })
+        
+            matchingItem.deliveryOptionsId = deliveryOptionId
+            this.saveToStorage()
+        }
+    };
+
+    return cart
+}
+
+const cart = Cart('cart-oop');
+const businessCart = Cart('cart-business');
+
+cart.loadFromStorage();
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,7 @@
 import { updatePage } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+//! to import the cart object
+//todo: import '../data/cart-oop.js'
 
 updatePage()
 renderPaymentSummary()
