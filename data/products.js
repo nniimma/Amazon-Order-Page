8ab593bd46ad672523060ffad6bc31f3c7aebbfe
Nima <alireza.khazforoosh@adventistas.org>
2024-08-08 10:26:05 -0300

    built-in classes: - new Date() - to get time: toLocaleTimeString()

--- data/products.js
+++ data/products.js
@@ -65,6 +65,11 @@ class Clothing extends Product{
   }
 }
 
+/* 
+todo: const date = new Date();
+todo: const time = date.toLocaleTimeString();
+*/
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
