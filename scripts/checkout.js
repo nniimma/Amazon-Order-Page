d351d75a283b10e362fa1400c563d523c11a8988
Nima <alireza.khazforoosh@adventistas.org>
2024-08-13 10:20:44 -0300

    using Promise  class to receive api

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,14 @@ import { updatePage } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 
-loadProducts(() => {
+// promis is a class and resolve is a function that tell when to go to the next step
+// if there is a code that should work after the promise is done, we put it in the then() function
+// when we use promise it work symultanously with the rest of the code after promise
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+}).then(() => {
     updatePage();
     renderPaymentSummary();
 });
