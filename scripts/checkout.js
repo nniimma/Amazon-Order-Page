b8694d0169d17c1a75657e5f12b5de210cdc0036
Nima <alireza.khazforoosh@adventistas.org>
2024-08-13 10:47:39 -0300

    run multiple promise at the same time with promise.all()

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,21 +3,19 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
-// promis is a class and resolve is a function that tell when to go to the next step
-// if there is a code that should work after the promise is done, we put it in the then() function
-// when we use promise it work symultanously with the rest of the code after promise
-new Promise((resolve) => {
-    loadProducts(() => {
-        resolve('sending values to next step');
-    });
-}).then((value) => {
-    console.log(value);
-    return new Promise((resolve) => {
+
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('sending values to next step');
+        });
+    }),
+    new Promise((resolve) => {
         loadCart(() => {
             resolve()
         })
     })
-}).then((value) => {
+]).then(() => {
     updatePage();
     renderPaymentSummary();
 });
