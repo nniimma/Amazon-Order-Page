996542d8bf4a33e794576a5dc81065e1fd276708
Nima <alireza.khazforoosh@adventistas.org>
2024-08-13 11:11:04 -0300

    using Fetch instead of XMLHttpRequest() - fetch returns a promise - after using the function of fetch we can use .then() to add more steps - fetch returns an array which doesnt need to .parse it

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,15 +1,11 @@
 import { updatePage } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
 
 Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve('hi');
-        });
-    }),
+    loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
             resolve('bye')
