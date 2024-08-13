d6ed8bf96d9ba4c0d6ecc493ae4aa27f739dc5f2
Nima <alireza.khazforoosh@adventistas.org>
2024-08-13 10:42:03 -0300

    sending value from each step of the Promise to another one

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -8,15 +8,16 @@ import { loadCart } from "../data/cart.js";
 // when we use promise it work symultanously with the rest of the code after promise
 new Promise((resolve) => {
     loadProducts(() => {
-        resolve();
+        resolve('sending values to next step');
     });
-}).then(() => {
+}).then((value) => {
+    console.log(value);
     return new Promise((resolve) => {
         loadCart(() => {
             resolve()
         })
     })
-}).then(() => {
+}).then((value) => {
     updatePage();
     renderPaymentSummary();
 });
